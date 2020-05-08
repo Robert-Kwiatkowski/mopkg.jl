2699d7dd7658f18141389457868a6969029ee1aa
Robert-Kwiatkowski <s169655@student.pg.edu.pl>
2020-05-08 17:52:00 +0200

    Added CI test and code coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using mopkg, Test
 
 @testset "bisection" begin
-	@test bisection(x->x^2-2, 1.413, 1.415) == 1.4142074584960938
+	@test bisection(x->x^2-2, 1.413, 1.415) == 1.4142109375
     @test bisection(x->x^2-2, 0, 2) == 1.4142074584960938
     @test bisection(x->x+5, -10, 100) == -4.999998211860657
 end
