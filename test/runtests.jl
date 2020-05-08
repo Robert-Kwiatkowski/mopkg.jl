0188313d24ca82e37c8fe344497aa33191056f37
Robert-Kwiatkowski <s169655@student.pg.edu.pl>
2020-05-08 17:37:15 +0200

    Test CI and coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,7 @@
 using mopkg, Test
 
 @testset "bisection" begin
-	@testset "Non-negative values" begin
-	@test bisection(x->x^2-2, 5, 10)
-	end
+	@test bisection(x->x^2-2, 1.4142074584960938, 1.4142074584960939) == 1.4142074584960938
     @test bisection(x->x^2-2, 0, 2) == 1.4142074584960938
     @test bisection(x->x+5, -10, 100) == -4.999998211860657
 end
